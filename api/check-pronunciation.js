fdb7373688d7437dabd2b5cc877df7f463d05838
Fantastic991 <fantasticcenter1@gmail.com>
2025-10-06 12:33:15 +0700

    Delete api/check-pronunciation.js

--- api/check-pronunciation.js
+++
@@ -1,5 +0,0 @@
-export default async function handler(req, res) {
-// สมมติเป็นระบบตรวจเสียง (ตอนนี้จะส่งคะแนนแบบสุ่ม)
-const score = Math.floor(Math.random() * 40) + 60;
-res.status(200).json({ score });
-}
